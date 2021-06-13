de43ec62026dab23de2c8d6b647620b2686c939c
Musab36 <musabhd@gmail.com>
2021-06-13 16:39:14 +0200

    initial commit

---
+++ app/src/main/java/com/salajim/musab/maalimmustafa/MainActivity.java
@@ -0,0 +1,14 @@
+package com.salajim.musab.maalimmustafa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
